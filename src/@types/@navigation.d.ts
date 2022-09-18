861b1e4751e6842c159d429fa746d64c64ebb7ae
Emerson Trindade <trinddade29@gmail.com>
2022-09-18 19:28:42 -0300

    add routes

---
+++ src/@types/@navigation.d.ts
@@ -0,0 +1,14 @@
+export interface GamesParms {
+  id: string;
+  title: string;
+  bannerUrl: string;
+}
+
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined;
+      game: GamesParms;
+    }
+  }
+}
